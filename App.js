5df2f4fa9c5b1934f5c6d44e6b7dd5dda791202e
abdullateef97 <adeniranyusufabdullateef@gmail.com>
2018-02-13 18:29:13 +0100

    fg

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase'
 import {Header} from './src/comp/common';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    firebase.initializeApp(
+      {
+        apiKey: "AIzaSyC49cXos0avB-fjkx2LWRNTndaPGhunPHI",
+        authDomain: "auth-a7ccb.firebaseapp.com",
+        databaseURL: "https://auth-a7ccb.firebaseio.com",
+        projectId: "auth-a7ccb",
+        storageBucket: "auth-a7ccb.appspot.com",
+        messagingSenderId: "109423552925"
+      }
+    )
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -16,7 +29,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- src/comp/common/Header.js
+++ src/comp/common/Header.js
@@ -27,7 +27,8 @@ const styles = {
         shadowColor : '#000',
         shadowOffset : {width : 0, height:3},
         shadowOpacity : 0.3,
-        elevation :2
+        elevation :2,
+        
 
     }
 }
